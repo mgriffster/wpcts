f1c82ea989653ddbea86efa4d691d469105fc6e9
MGriffith <mgriffith@xpressdocs.com>
2019-11-05 22:36:15 -0600

    First draft

---
+++ index.js
@@ -0,0 +1,121 @@
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+const cookieParser = require('cookie-parser');
+var pgp = require('pg-promise')();
+var db = pgp('postgres://ujrqwyhfbscbgs:87516f23130cec74bd5acb014b58c5528b072a5507d705c140bccd254e6f7d8e@ec2-54-197-238-238.compute-1.amazonaws.com:5432/dos8rg55607fp?ssl=true');
+
+var jsonParser = bodyParser.json();
+
+var app = express();
+app.use(express.static(__dirname));
+app.use('js', express.static('views/js'));
+app.use(cookieParser('sumoSecret'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'sumoSecret',
+    name: 'sumoCookie',
+    proxy: true,
+    resave: true,
+    saveUninitialized: true
+}));
+app.set('stylesheets', express.static('views/stylesheets'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    if(req.session !== undefined && req.session.sumoName !== undefined)
+    {
+        res.render('pages/home');
+    }
+    else{
+        res.render('pages/login');
+    }
+    
+});
+
+app.get('/home', function(req,res){
+    if(req.session !== undefined && req.session.sumoName !== undefined)
+    {
+        res.render('pages/home');
+    }
+    else{
+        res.redirect('/');
+        res.send();
+    }
+    
+});
+
+app.post('/login', function(req,res){
+        var login = {
+            "name":req.body.username,
+            "password":req.body.password
+        }
+        db.any('SELECT * FROM user_info WHERE user_name = $1', login.name).then(function(data){
+            if(data[0].password == login.password)
+            {
+                login.success = true;
+                req.session['sumoName'] = login.name;
+                res.send(login);
+            }
+            else
+            {
+                login.success = false;
+                res.send(login);
+            }
+        });
+        
+});
+
+app.post('/create', function(req,res){
+    var user = {
+        "name": req.body.username,
+        "password": req.body.password,
+        "email": req.body.email
+    }
+    db.any('SELECT * FROM user_info WHERE user_name = $1', user.name).then(function(data){
+        if(data.length > 0)
+        {
+            user.success = false;
+            res.send(user);
+        }
+        else
+        {
+            addUser(user);
+            user.success = true;
+            req.session['sumoName'] = user.name;
+            res.send(user);
+        }
+    });
+});
+
+app.get('/rikishi', function(req,res){
+    if(req.session !== undefined && req.session.sumoName !== undefined)
+    {
+        res.render('pages/rikishi');
+    }
+    else{
+        res.redirect('/');
+        res.send();
+    }
+});
+
+app.get('/logout', function(req,res){
+    req.session.destroy();
+    res.redirect('/');
+    res.send();
+});
+
+app.listen(PORT, () => console.log('Listening on ' + PORT));
+
+function addUser(userInfo){
+    db.one('INSERT INTO user_info(user_name, password, email) VALUES($1, $2, $3) RETURNING user_name', [userInfo.name, userInfo.password, userInfo.email]).then(function(data){
+        console.log('New User Added' + data.user_name);
+    }).catch(error => {
+        console.log(error);   
+    });
+}
---
+++ js/login.js
@@ -0,0 +1,55 @@
+
+
+window.onload = function() {
+    document.getElementById("createAccount").onclick = toggleCreate;
+    document.getElementById("signin").onclick = toggleSignin;
+    document.getElementById("login").onclick = login;
+    this.document.getElementById("create").onclick = create;
+};
+
+
+function toggleCreate() {
+    document.getElementById("register-form").style.display = 'block';
+    document.getElementById("login-form").style.display = 'none';
+}
+
+function toggleSignin() {
+    document.getElementById("register-form").style.display = 'none';
+    document.getElementById("login-form").style.display = 'block';
+}
+
+function login(event){
+    event.preventDefault();
+    var name = document.getElementById("username").value;
+    var pw = document.getElementById("password").value;
+    $.post('/login', { username: name, password : pw}).done(function(data){
+        if(data.success !== undefined && !data.success)
+        {
+            document.getElementById("password").value = '';
+        }
+        else if(data.success !== undefined && data.success == true)
+        {
+            window.location.href = '/home';
+        }
+    });
+    return false;
+}
+
+function create(event){
+    event.preventDefault();
+    var name = document.getElementById("newusername").value;
+    var pw = document.getElementById("newpassword").value;
+    var em = document.getElementById("newemail").value;
+
+    $.post('/create', {username:name, password:pw, email:em}).done(function(data){
+        if(data.success !== undefined && !data.success)
+        {
+            alert('Unsuccessful account creation. Ask McMichael or just give up.');
+        }
+        else if(data.success !== undefined && data.success == true)
+        {
+            window.location.href = '/home';
+        }
+    });
+    return false;
+}
